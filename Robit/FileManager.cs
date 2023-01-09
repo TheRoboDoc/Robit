63e20ddac8c1105a95856af4cc7397c63c4dc109
TheRoboDoc <maki.eriks@gmail.com>
2023-01-09 02:55:21 +0200

    Version 0.3.1 - Removed not needed components

--- Robit/FileManager.cs
+++ Robit/FileManager.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Security.Cryptography;
+﻿using System.Text.Json;
 
 namespace Robit
 {
