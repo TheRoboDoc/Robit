0c800500dec6c559b641b96fc55682984b5e4994
RoboDoc <18618265+TheRoboDoc@users.noreply.github.com>
2023-07-06 22:34:54 +0300

    Added a base for future text based adventure module

---
+++ Robit/TextAdventure/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robit.TextAdventure
+{
+    public class GameManager
+    {
+        public enum GameMode
+        {
+            Private,
+            Public
+        }
+
+        public struct Event
+        {
+            public string Description;
+            public string Question;
+            public string Answer;
+            public string Result;
+        }
+
+        private DiscordMember[] players;
+
+        private GameMode gamemode;
+
+        private uint turnCount;
+
+        private List<Event> events;
+
+        public GameManager(DiscordMember[] players, GameMode gameMode)
+        {
+            this.players = players;
+            this.gamemode = gameMode;
+            this.turnCount = 0;
+            this.events = new List<Event>();
+        }
+    }
+}
